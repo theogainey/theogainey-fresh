4bf14783f75324d869cd89fc9c4f318716ab318b
Theo Gainey <theogainey@gmail.com>
2022-09-08 22:15:33 -0400

    feat: favicons

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -12,27 +12,26 @@ export default function App(props: AppProps) {
           href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap'
           rel='stylesheet'
         />
-        <link href='/favicons/favicon.ico' rel='shortcut icon' />
-        <link href='/favicons/site.webmanifest' rel='manifest' />
+        <link href={asset('/favicons/favicon.ico')} rel='shortcut icon' />
+        <link href={asset('/favicons/site.webmanifest')} rel='manifest' />
         <link
-          href='/favicons/apple-touch-icon.png'
+          href={asset('/favicons/apple-touch-icon.png')}
           rel='apple-touch-icon'
           sizes='180x180'
         />
         <link
-          href='/favicons/favicon-32x32.png'
+          href={asset('/favicons/favicon-32x32.png')}
           rel='icon'
           sizes='32x32'
           type='image/png'
         />
         <link
-          href='/favicons/favicon-16x16.png'
+          href={asset('/favicons/favicon-16x16.png')}
           rel='icon'
           sizes='16x16'
           type='image/png'
         />
-        {
-          /* <script
+        <script
           async
           src='https://www.googletagmanager.com/gtag/js?id=G-VERG9WQXHD'
         >
@@ -44,8 +43,7 @@ export default function App(props: AppProps) {
             gtag('js', new Date());
             gtag('config', 'G-VERG9WQXHD');`,
           }}
-        />*/
-        }
+        />
       </Head>
       <props.Component />
     </>
