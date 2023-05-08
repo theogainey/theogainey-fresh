d804444f484636a9aa023894e9c7ab4aff46dea7
Theo Gainey <theo@heysparkbox.com>
2023-05-08 08:49:50 -0400

    fix: formatting and spellcheck wordlist

--- components/WorkPreview.tsx
+++ components/WorkPreview.tsx
@@ -6,18 +6,18 @@ type WorkPreviewProps = {
 
 export default function WorkPreview({ posts }: WorkPreviewProps) {
   return (
-      <ul class='cmp-work-preview'>
-        {posts.map((post) => (
-          <li>
-            <a class='cmp-work-preview__card' href={`/${post.uri}`}>
-              <h3 class='cmp-work-preview__title'>{post.title}</h3>
-              <p class='cmp-work-preview__preview'>{post.preview}</p>
-              <time class='cmp-work-preview__date' dateTime={post.dateTime}>
-                {post.date}
-              </time>
-            </a>
-          </li>
-        ))}
-      </ul>
+    <ul class='cmp-work-preview'>
+      {posts.map((post) => (
+        <li>
+          <a class='cmp-work-preview__card' href={`/${post.uri}`}>
+            <h3 class='cmp-work-preview__title'>{post.title}</h3>
+            <p class='cmp-work-preview__preview'>{post.preview}</p>
+            <time class='cmp-work-preview__date' dateTime={post.dateTime}>
+              {post.date}
+            </time>
+          </a>
+        </li>
+      ))}
+    </ul>
   );
 }
