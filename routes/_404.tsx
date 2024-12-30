851e02037a206e1c359b7fd24be5be5792ae1da1
Theo Gainey <theogainey@gmail.com>
2024-12-30 12:56:42 -0500

    fix linting error

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -2,7 +2,7 @@ import { UnknownPageProps } from '$fresh/server.ts';
 import Layout from '../components/Layout.tsx';
 import NotFound from '../components/NotFound.tsx';
 
-export default function NotFoundPage({ url }: UnknownPageProps) {
+export default function NotFoundPage({ _url }: UnknownPageProps) {
   return (
     <Layout>
       <NotFound />
